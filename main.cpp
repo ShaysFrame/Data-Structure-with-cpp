64ad03a11e5d0b49b7b8dbfa3a95f8fd242a59d5
ShaysFrame <akashshaykh@gmail.comm>
2022-06-19 22:51:48 +0800

    Commiting some changes in the folder structures

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
-// Fraction Class
+//introduction to recursion
+
 #include <iostream>
+
+int factorial(int n)
+{
+    int smallOutput = factorial (n-1);
+    return n * smallOutput;
+}
 
 int main()
 {
-    std::cout << "Hello world";
+    int n{};
+    std::cin >> n;
+
+    int output = factorial(n);
+    std::cout << output << std::endl;
 }
